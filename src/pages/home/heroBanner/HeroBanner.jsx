5fe97424db07349f64eb0f52e276b06baf9b7086
sumangitron <sumanabatar1994@gmail.com>
2024-06-09 21:51:38 +0530

    make search button fuctional

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -50,7 +50,7 @@ const HeroBanner = () => {
               onKeyUp={searchQueryHandler}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button>Search</button>
+            <button onClick={() => navigate(`/search/${query}`)}>Search</button>
           </div>
         </div>
       </ContentWrapper>
